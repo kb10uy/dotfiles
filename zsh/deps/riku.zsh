948ab93006175227c51b1ef32f936ae98f46211d
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-05-13 16:49:44 +0900

    iTerm2で透過するようにする

--- zsh/deps/riku.zsh
+++ zsh/deps/riku.zsh
@@ -3,3 +3,6 @@ alias dircolors='gdircolors'
 export NVM_DIR="$HOME/.nvm"
 source /usr/local/opt/nvm/nvm.sh
 eval $(thefuck --alias)
+if [[ $TERM_PROGRAM == 'iTerm.app' ]]; then
+  export TRANSPARENTTERM='iterm'
+fi
