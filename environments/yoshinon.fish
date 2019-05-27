f49e35d08ad5a965d8972e274b8ee274f9accfd5
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-05-28 00:56:47 +0900

    yoshinon を fish に移行

---
+++ environments/yoshinon.fish
@@ -0,0 +1,23 @@
+set -x EDITOR 'nvim'
+set -x VISUAL 'nvim'
+
+set -U fish_user_paths \
+  $HOME/.cargo/bin \
+  $HOME/.opam/system/bin \
+  $HOME/.local/bin \
+  $HOME/.config/composer/vendor/bin \
+  $HOME/.local/share/node_modules/bin \
+  /usr/local/texlive/2019/bin/x86_64-linux \
+  /usr/local/go/bin \
+  /usr/local/ssl/bin \
+  /usr/local/bin \
+  /usr/bin \
+  /bin \
+  /sbin
+
+set -x GPG_TTY (tty)
+if [ -n "$SSH_CONNECTION" ]
+  set -x PINENTRY_USER_DATA 'USE_CURSES=1'
+end
+
+eval (opam env)
