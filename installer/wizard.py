c81b504071dd73cf08b2836658be57e97cd1f3bc
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-06-12 00:10:00 +0900

    コマンドチェックを追加

--- installer/wizard.py
+++ installer/wizard.py
@@ -1,4 +1,5 @@
 from os import path, getenv
+import shutil
 from installer import console
 from installer.config import Configuration
 
@@ -21,6 +22,11 @@ class Wizard:
         """
         console.banner()
         self.__check_repository()
+        self.__check_command('git')
+        self.__check_command('python3')
+        self.__check_command('pip3')
+        self.__check_command('fish')
+        self.__check_command('nvim')
 
     def __check_repository(self):
         """
@@ -35,4 +41,21 @@ class Wizard:
             console.ng()
             console.fatal('Invalid repository location: {}'.format(base_dir))
             console.fatal('Place dotfiles repository as {}'.format(expected), True)
+
+    def __check_command(seld, cmd: str):
+        """
+        指定されたコマンドが存在するか調べる
+        無かったときは死ぬ
+
+        Parameters
+        ----------------
+        cmd: str
+            コマンド名
+        """
+        console.waiting('Checking whether \'{}\' exists'.format(cmd))
+        if shutil.which(cmd) is not None:
+            console.ok()
+        else:
+            console.ng()
+            console.fatal('{} was not found. Install it via any means.'.format(cmd))
 
