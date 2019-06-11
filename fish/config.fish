dbc1f1ad93e6b6f5a87e10205faa7b4b7eeb36df
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-06-11 22:07:52 +0900

    TRUECOLOR～～～！！！

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,10 @@
 set -g fisher_path ~/.local/share/fish/fisher
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-for file in $fisher_path/conf.d/*.fish
-    builtin source $file 2> /dev/null
-end
 
 set FISH_PLATFORM (uname)
+# Neovim のクソ野郎 $COLORTERM を握り潰しやがった
+set fish_term24bit 1
 
 # Load dependency file
 set -l dependency_file "$HOME/dotfiles/environments/"(hostname)".fish"
@@ -17,6 +16,7 @@ else
   echo "Dependency file for "(hostname)" was not found!"
 end
 
+# Change dircolors
 if [ -e "$HOME/.dir_colors" ]
   switch $FISH_PLATFORM
     case Linux
@@ -24,4 +24,9 @@ if [ -e "$HOME/.dir_colors" ]
     case Darwin
       eval (gdircolors -c $HOME/.dir_colors)
   end
+end
+
+# Load plugins
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
 end
