bcaee825121d72885b9e845bf25e7af5ea1a11d4
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-07-16 10:43:31 +0900

    export HOMEBREW_NO_AUTO_UPDATE=1

--- environments/riku.fish
+++ environments/riku.fish
@@ -3,6 +3,7 @@ set -x VISUAL 'nvim'
 set -x TRANSPARENTTERM 'iTerm2.app'
 set -x PYTHON3_PATH '/usr/local/bin/python3'
 set -x JAVA_HOME (/usr/libexec/java_home)
+set -x HOMEBREW_NO_AUTO_UPDATE 1
 
 set -x PATH \
   /usr/local/texlive/2019/bin/x86_64-darwin \
