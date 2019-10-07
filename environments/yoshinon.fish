d8e49a711372559cf953485ea04feeddbd716466
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-10-07 18:55:38 +0900

    Neovim でもローカルにインストールした fzf を使うようにした

--- environments/yoshinon.fish
+++ environments/yoshinon.fish
@@ -9,6 +9,7 @@ set -x PATH \
   $HOME/.local/bin \
   $HOME/.config/composer/vendor/bin \
   $HOME/.local/share/node_modules/bin \
+  $HOME/.fzf/bin \
   /usr/local/texlive/2019/bin/x86_64-linux \
   /usr/local/go/bin \
   /usr/local/ssl/bin \
